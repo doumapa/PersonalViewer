1326b66290047571e9d95a156bc5925b58a515e8
makoto kaneko <makoto.berry.kaneko@gmail.com>
2017-09-24 15:43:26 +0900

    base project

---
+++ PersonalViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PersonalViewer
+//
+//  Created by kaneko makoto on 2017/09/23.
+//  Copyright © 2017年 quiet branch studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
